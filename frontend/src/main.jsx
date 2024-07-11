452f3d9f357c2f4dc9324e8b3e95f120d177f673
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-07-11 07:11:07 +0530

    add single blog page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
 import { Layout } from './components'
-import { Home } from './pages'
+import { Home, Register, Login, SingleBlogPage } from './pages'
 import { createBrowserRouter, createRoutesFromChildren, Outlet, Route, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/single-blog' element={<SingleBlogPage />} />
     </Route>
   )
 )
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,6 @@
 import Home from "./Home.jsx"
+import Login from './Login.jsx'
+import Register from './Register.jsx'
+import SingleBlogPage from "./SingleBlogPage.jsx"
 
-export { Home }
+export { Home, Login, Register, SingleBlogPage }
