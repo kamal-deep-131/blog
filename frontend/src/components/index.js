f817effe27cd49aa7c1a3450e700d58105bf43ec
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-07-11 06:42:49 +0530

    Add Login Page & inputFieldComponent

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 import Layout from "./Layout.jsx";
 import Hero from './Hero.jsx'
+import InputField from './InputField.jsx'
 
-export { Navbar, Footer, Layout, Hero }
+export { Navbar, Footer, Layout, Hero, InputField }
